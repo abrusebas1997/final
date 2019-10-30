e13cf92ac2172ea40f623b35d64765c949f14d2c
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2019-10-30 11:56:30 -0700

    new

---
+++ js/style.js
@@ -0,0 +1,47 @@
+$(window).on("load", function (e) {
+    $(".image-wrapper").addClass("is-visible");
+});
+
+
+$('.resToggle').click(function () {
+    $('.the-nav').toggleClass('active');
+});
+
+const signupButton = document.getElementById('signup-button'),
+    loginButton = document.getElementById('login-button'),
+    userForms = document.getElementById('user_options-forms')
+
+/**
+ * Add event listener to the "Sign Up" button
+ */
+signupButton.addEventListener('click', () => {
+    userForms.classList.remove('bounceRight')
+    userForms.classList.add('bounceLeft')
+}, false)
+
+/**
+ * Add event listener to the "Login" button
+ */
+loginButton.addEventListener('click', () => {
+    userForms.classList.remove('bounceLeft')
+    userForms.classList.add('bounceRight')
+}, false)
+
+$(function () {
+    var selectedClass = "";
+    $(".filter").click(function () {
+        selectedClass = $(this).attr("data-rel");
+        $("#gallery").fadeTo(100, 0.1);
+        $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
+        setTimeout(function () {
+            $("." + selectedClass).fadeIn().addClass('animation');
+            $("#gallery").fadeTo(300, 1);
+        }, 300);
+    });
+});
+
+jQuery(document).ready(function ($) {
+    $('.type-it').typeIt({
+        content: 'I am a Web Developer'
+    });
+});
